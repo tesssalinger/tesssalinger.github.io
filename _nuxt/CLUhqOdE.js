7b72e4a5bf0c078a4df9f3c1f9b29218c5e572ac
jnabulsi <jordanabulsi@gmail.com>
2025-03-11 21:02:45 +1100

    mobile update

--- _nuxt/CLUhqOdE.js
+++ _nuxt/CLUhqOdE.js
@@ -1,4 +1,4 @@
-import{W as o,P as t}from"./aJyXDGXF.js";import{_ as n,c as a,b as e,a as s,o as r}from"./0oJv-gLA.js";const c={class:"photo-container"},m={class:"projects"},d={__name:"vfx",setup(g){return(f,i)=>(r(),a("div",c,[e(o,{videoSrc:"/video/vfx.mp4",title:"VFX & Video editing",subHeading:"[I work in the After Effects, Blender & Nuke]",text:`For years, I’ve been deeply immersed in the world of video design, refining my skills with a range of powerful tools.
+import{W as o,P as t}from"./DA107dej.js";import{_ as n,c as a,b as e,a as s,o as r}from"./Dgls1hIa.js";const c={class:"photo-container"},m={class:"projects"},d={__name:"vfx",setup(g){return(l,i)=>(r(),a("div",c,[e(o,{videoSrc:"/video/vfx.mp4",title:"VFX & Video editing",subHeading:"[I work in the After Effects, Blender & Nuke]",text:`For years, I’ve been deeply immersed in the world of video design, refining my skills with a range of powerful tools.
 From After Effects and Premiere Pro, to Adobe Illustrator, Blender, and Nuke, I’ve developed expertise in motion
 graphics, video editing, 3D modeling, and compositing. These programs have enabled me to craft visually engaging
 narratives and seamless visual experiences. My passion lies in bringing stories to life through the magic of moving
@@ -9,4 +9,4 @@ Masking : Premier Pro and Photoshop`}),e(t,{imageSrc:"/img/vfx4.webp",title:"BOR
 Projection Mapping : MadMapper
 Queues : Qlab`}),e(t,{imageSrc:"/img/vfx5.webp",title:"Evenings with Janine",subHeading:"Televsion show, Editor",text:`Sequencing multicam footage, effects,
 masking and credits : Premier Pro`}),e(t,{imageSrc:"/img/vfx6.webp",title:"First Run",subHeading:"Televsion show, Editor",text:`Sequencing multicam footgae, effects,
-masking and credits : Premier Pro`})])]))}},u=n(d,[["__scopeId","data-v-4eaf2169"]]);export{u as default};
+masking and credits : Premier Pro`})])]))}},u=n(d,[["__scopeId","data-v-77901586"]]);export{u as default};
--- _nuxt/DkfzYGMk.js
+++ _nuxt/DkfzYGMk.js
@@ -1,4 +1,4 @@
-import{W as o,P as i}from"./aJyXDGXF.js";import{_ as a,c as s,b as e,a as t,o as r}from"./0oJv-gLA.js";const g={class:"photo-container"},l={class:"projects"},d={__name:"design",setup(c){return(m,n)=>(r(),s("div",g,[e(o,{videoSrc:"/video/",title:"3D & Game Design",subHeading:"[I work in Blender, Maya, Photoshop, Substance Painter, Unity and Visual Studio Code]",text:`Over the last few years I have immersed herself in the world of 3D design, learning tools like Maya, Blender, Illustrator,
+import{W as o,P as i}from"./DA107dej.js";import{_ as a,c as s,b as e,a as t,o as r}from"./Dgls1hIa.js";const g={class:"photo-container"},l={class:"projects"},d={__name:"design",setup(c){return(m,n)=>(r(),s("div",g,[e(o,{videoSrc:"/video/",title:"3D & Game Design",subHeading:"[I work in Blender, Maya, Photoshop, Substance Painter, Unity and Visual Studio Code]",text:`Over the last few years I have immersed herself in the world of 3D design, learning tools like Maya, Blender, Illustrator,
 Substance Painter, Photoshop, and Unity.From texturing and modeling to shadingand sculpting, my aim is to bring digital
 creations to life. I have explored the exciting realm of game development,blending creativity with technical expertise to craft
 immersive experience.`}),t("div",l,[n[0]||(n[0]=t("p",{class:"section-title"},"Projects",-1)),e(i,{imageSrc:"/img/Design/design1.png",title:"Reminisce",subHeading:"Click through dialogue Game",text:`3D modelling, UV unwrapping, Sculpting : Maya
@@ -10,4 +10,4 @@ Sculpting & Texturing: Blender
 Real life footage: Green Screen & DSLR`}),e(i,{imageSrc:"/img/Design/design3.png",title:"Dancing Jellyfish",subHeading:"3D animation",text:`3D modelling, UV unwrapping,
 Sculpting & Texturing: Blender`}),e(i,{imageSrc:"/img/Design/design4.png",title:"I must be Dreaming Computer",subHeading:"3D animation",text:`3D modelling, UV unwrapping,
 Sculpting, Texturing and Compositing: Blender
-Video Design : After effects`})])]))}},h=a(d,[["__scopeId","data-v-19bcde95"]]);export{h as default};
+Video Design : After effects`})])]))}},f=a(d,[["__scopeId","data-v-f3bec7d6"]]);export{f as default};
